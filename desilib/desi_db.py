2b7aa233ac4dcfd44c53109504ce47a4708d19f1
Nao Suzuki <nao.suzuki@gmail.com>
2025-06-19 18:30:55 -0400

    update

--- desilib/desi_db.py
+++ desilib/desi_db.py
@@ -22,7 +22,9 @@ class zall():
           self.rows=numpy.arange(self.nspec)
 
       def readstar(self):
-          columns=['PLATE','MJD','FIBERID','OBJTYPE','CLASS'=='STAR','SUBCLASS']
+          columns=['TARGETID','LASTNIGHT','Z','ZERR','ZWARN','CHI2',\
+          'SPECTYPE','SUBTYPE'=='STAR','SUBCLASS']
+
           d=fitsio.read(self.fitstablename,columns=columns,rows=self.rows)
           self.platelist=d['PLATE']
           self.mjdlist=d['MJD']
@@ -34,9 +36,12 @@ class zall():
       def readtest(self):
           columns=['TARGETID','SURVEY','PROGRAM',\
           'FIRSTNIGHT','LASTNIGHT',\
-          'Z','ZERR','ZWARN',\
-          'TILEID','TARGET_RA','TARGET_DEC','SPECTYPE','PRIORITY',\
-          'DEVICE_LOC','PETAL_LOC','LOCATION','FIBER']
+          'Z','ZERR','ZWARN','CHI2','NPIXELS','LAMBDA_REF',\
+          'SPECTYPE','SUBTYPE','PRIORITY',\
+          'TILEID','TARGET_RA','TARGET_DEC',
+          'DEVICE_LOC','PETAL_LOC','LOCATION','FIBER',\
+          'REF_ID','REF_CAT',\
+          'COADD_NUMEXP','COADD_EXPTIME']
           print('Reading ',self.fitstablename)
 #          print('Number of Spectra is ',len(self.nspec))
           d=fitsio.read(self.fitstablename,columns=columns,rows=self.rows)
@@ -51,16 +56,25 @@ class zall():
           self.z=d['Z']
           self.zerr=d['ZERR']
           self.zwarn=d['ZWARN']
+          self.chisq=d['CHI2']
+          self.npix=d['NPIXELS']
 
+          self.spectype=d['SPECTYPE']
+          self.subtype=d['SUBTYPE']
+
+          self.lambda_ref=d['LAMBDA_REF']
           self.tileid=d['TILEID']
           self.ra=d['TARGET_RA']
           self.dec=d['TARGET_DEC']
-          self.spectype=d['SPECTYPE']
           self.device_loc=d['DEVICE_LOC']
           self.petal_loc=d['PETAL_LOC']
           self.priority=d['PRIORITY']
           self.location=d['LOCATION']
           self.fiber=d['FIBER']
+          self.ref_id=d['REF_ID']
+          self.ref_cat=d['REF_CAT']
+          self.coadd_numexp=d['COADD_NUMEXP']
+          self.coadd_exptime=d['COADD_EXPTIME']
           #self.subclass=d['SUBCLASS']
           #self.specprimary=d['SPECPRIMARY']
 
@@ -77,14 +91,25 @@ print('lastnight',zall.lastnight)
 print('z',zall.z)
 print('zerr',zall.zerr)
 print('zwarn',zall.zwarn)
+print('chi2',zall.chisq)
+print('npixels',zall.npix)
+print('lambda_ref',zall.lambda_ref)
 
 print('tileid',zall.tileid)
 print('ra',zall.ra)
 print('dec',zall.dec)
 print('priority',zall.priority)
 print('spectype',zall.spectype)
+print('Unique Object Type',numpy.unique(zall.spectype))
+print('subtype',zall.subtype)
+print('Unique Subtype',numpy.unique(zall.subtype))
 print('location',zall.location)
 print('device_loc',zall.device_loc)
 print('petal_loc',zall.petal_loc)
 print('fiber',zall.fiber)
+
+print('ref_id',zall.ref_id)
+print('ref_cat',zall.ref_cat)
+print('numexp',zall.coadd_numexp)
+print('exptime',zall.coadd_exptime)
 #print('tileid',zall.tileid)
