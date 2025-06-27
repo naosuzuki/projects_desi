1423180d2eac18deafd0ebf1c2892e0e241a7469
Nao Suzuki <nao.suzuki@gmail.com>
2025-06-27 15:56:41 -0400

    update

--- programs/01_arrange_zallforGAIA.py
+++ programs/01_arrange_zallforGAIA.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import os
 
-def extract_stars(csvfile):
+def extract_stars(csvfile,output):
    df=pd.read_csv(csvfile,dtype={'targetid':int,'ra':'float32','dec':'float32','spectype':str})
    print(len(df))
 
    dfstar1=df[(df['spectype']=='STAR') & (df['targetid']>0)]
    print(len(dfstar1))
    dfstar2=dfstar1[['targetid','ra','dec']]
-   dfstar2.to_csv('desidr2star_float32.csv',index=False)
+   dfstar2.to_csv(output,index=False)
 #   dfstar2[0:5000000].to_csv('desidr2star1_float32.csv',index=False)
 #   dfstar2[5000001:].to_csv('desidr2star2_float32.csv',index=False)
 
 # DR1
 csvfile=os.environ['DESI_REDUX']+'zall-tilecumulative-iron.csv'
+output='../csvfiles/desidr1star_float32.csv'
 # DR2
 csvfile=os.environ['DESI_REDUX']+'zall-tilecumulative-loa.csv'
+output='../csvfiles/desidr2star_float32.csv'
 print(csvfile)
-extract_stars(csvfile)
+extract_stars(csvfile,output)
