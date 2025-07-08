98c4b5e416a92b165f6647138873aa28ca473c7b
Nao Suzuki <nao.suzuki@gmail.com>
2025-07-08 15:41:50 -0400

    update

--- programs/01_read_zall.py
+++ programs/01_read_zall.py
@@ -23,9 +23,32 @@ df=pd.DataFrame({'targetid':zall.targetid,\
 		 'coadd_numtile':zall.coadd_numtile,\
                  'coadd_exptime':zall.coadd_exptime})
 
-dfstar=df[df['spectype']=='STAR']
-dfstar.to_csv('zalldr2pixstar.csv',index=False)
 df.to_csv('zalldr2pix.csv',index=False)
+
+del df
+
+df=pd.read_csv('zalldr2pix.csv')
+dfstar=df[df['spectype']=='STAR']
+dfstar.to_csv('dfstar.csv',index=False)
+
+sys.exit(1)
+
+#df['spectype'] = df['spectype'].astype(str)
+#dfstar=df[df['spectype']=='STAR']
+#for col in df.columns:
+#    print(col, df[col].dtype)
+
+# Force the 'spectype' column to be native-endian string
+#df['spectype'] = df['spectype'].astype(str)
+#df['survey'] = df['survey'].astype(str)
+#df['program'] = df['program'].astype(str)
+#df['objtype'] = df['objtype'].astype(str)
+
+#for col in df.columns:
+#    print(col, df[col].dtype)
+# Now filtering works safely
+#dfstar=df[df['spectype']=='STAR']
+#dfstar.to_csv('zalldr2pixstar.csv',index=False)
 
 #spall=sdss_catalog.spall()
 #print(spall.dr)
